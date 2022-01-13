112284ca6459da7403bb88043d06d758fbb0a4bb
Ashwin Arora <ashwinarora48@gmail.com>
2022-01-13 14:14:38 +0530

    updated whitelist interface

--- contracts/IWhiteList.sol
+++ contracts/IWhiteList.sol
@@ -8,4 +8,6 @@ interface IWhiteList {
     function Register(address _Subject,uint256 _Id,uint256 _Amount) external;
     function IsNeedRegister(uint256 _Id) external view returns(bool);
     function LastRoundRegister(address _Subject,uint256 _Id) external;
+    function CreateManualWhiteList(uint256 _ChangeUntil, address _Contract) external payable returns(uint256 Id);
+    function ChangeCreator(uint256 _Id, address _NewCreator) external;
 }
