0aa4b3daaed3e33cffa1e1260a9c6263a2da8e4e
Ashwin Arora <ashwinarora48@gmail.com>
2022-02-09 14:59:30 +0530

    removed useless function from IWhitelist

--- contracts/IWhiteList.sol
+++ contracts/IWhiteList.sol
@@ -6,7 +6,6 @@ pragma solidity ^0.6.0;
 interface IWhiteList {
     function Check(address _Subject, uint256 _Id) external view returns(uint);
     function Register(address _Subject,uint256 _Id,uint256 _Amount) external;
-    function IsNeedRegister(uint256 _Id) external view returns(bool);
     function LastRoundRegister(address _Subject,uint256 _Id) external;
     function CreateManualWhiteList(uint256 _ChangeUntil, address _Contract) external payable returns(uint256 Id);
     function ChangeCreator(uint256 _Id, address _NewCreator) external;
