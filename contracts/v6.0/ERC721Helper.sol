dd32dbef495deac87e401e7bc1087c1498a6dcc4
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2022-04-14 12:11:36 +0300

    added v6 helper for old contracts

---
+++ contracts/v6.0/ERC721Helper.sol
@@ -0,0 +1,52 @@
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "openzeppelin-solidity/contracts/token/ERC721/IERC721.sol";
+
+contract ERC721Helper {
+    event TransferOut(address Token, uint256 TokenId, address To);
+    event TransferIn(address Token, uint256 TokenId, address From);
+
+    modifier TestNFTAllownce(
+        address _token,
+        uint256 _tokenId,
+        address _owner
+    ) {
+        require(
+            IERC721(_token).isApprovedForAll(_owner, address(this)) ||
+            IERC721(_token).getApproved(_tokenId) == address(this),
+            "No Allowance"
+        );
+        _;
+    }
+
+    function TransferNFTOut(
+        address _Token,
+        uint256 _TokenId,
+        address _To
+    ) internal {
+        IERC721(_Token).transferFrom(address(this), _To, _TokenId);
+        emit TransferOut(_Token, _TokenId, _To);
+        assert(IERC721(_Token).ownerOf(_TokenId) == _To);
+    }
+
+    function TransferNFTIn(
+        address _Token,
+        uint256 _TokenId,
+        address _From
+    ) internal TestNFTAllownce(_Token, _TokenId, _From) {
+        IERC721(_Token).transferFrom(_From, address(this), _TokenId);
+        emit TransferOut(_Token, _TokenId, _From);
+        assert(IERC721(_Token).ownerOf(_TokenId) == address(this));
+    }
+
+    function SetApproveForAllNFT(
+        address _Token,
+        address _To,
+        bool _Approve
+    ) internal {
+        IERC721(_Token).setApprovalForAll(_To, _Approve);
+    }
+}
