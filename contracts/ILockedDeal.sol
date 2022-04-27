29752c5ee10fbf7924a70ae04f245bd851acd151
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2022-04-27 11:35:13 +0300

    updated ILockedDeal Interface for V2 version

--- contracts/ILockedDeal.sol
+++ contracts/ILockedDeal.sol
@@ -2,6 +2,13 @@
 pragma solidity ^0.8.0;
 
 interface ILockedDeal {
-    function CreateNewPool(address _Token, uint64 _FinishTime, uint256 _StartAmount, address _Owner) external returns(uint256);
-    function WithdrawToken(uint256 _PoolId) external returns(bool);
+    function CreateNewPool(
+        address _Token,
+        uint256 _StartTime,
+        uint256 _FinishTime,
+        uint256 _StartAmount,
+        address _Owner
+    ) external returns (uint256);
+
+    function WithdrawToken(uint256 _PoolId) external returns (bool);
 }
