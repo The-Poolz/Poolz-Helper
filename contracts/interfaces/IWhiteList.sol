0f35888070add2b73f2a9924778a3f4ef290800a
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2022-05-12 12:30:50 +0300

    whitelist updated

--- contracts/interfaces/IWhiteList.sol
+++ contracts/interfaces/IWhiteList.sol
@@ -9,4 +9,6 @@ interface IWhiteList {
     function LastRoundRegister(address _Subject,uint256 _Id) external;
     function CreateManualWhiteList(uint256 _ChangeUntil, address _Contract) external payable returns(uint256 Id);
     function ChangeCreator(uint256 _Id, address _NewCreator) external;
+    function AddAddress(uint256 _Id, address[] calldata _Users) external;
+    function RemoveAddress(uint256 _Id, address[] calldata _Users) external;
 }
