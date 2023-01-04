1c211b40ad0870d0a67027294c3ebe1e4eecd5da
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-01-04 15:06:12 +0200

    update ILockedDealV2 interface

--- contracts/interfaces/ILockedDealV2.sol
+++ contracts/interfaces/ILockedDealV2.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.0;
 
 interface ILockedDealV2 {
     function CreateNewPool(
-        address _Token,
-        uint256 _StartTime,
-        uint256 _FinishTime,
-        uint256 _StartAmount,
-        address _Owner
-    ) external returns (uint256);
+        address _Token, //token to lock address
+        uint256 _StartTime, //Until what time the pool will start
+        uint256 _CliffTime, //Before CliffTime can't withdraw tokens
+        uint256 _FinishTime, //Until what time the pool will end
+        uint256 _StartAmount, //Total amount of the tokens to sell in the pool
+        address _Owner // Who the tokens belong to
+    ) external payable;
 
-    function WithdrawToken(uint256 _PoolId) external returns (bool);
+    function WithdrawToken(uint256 _PoolId)
+        external
+        returns (uint256 withdrawnAmount);
 }
