02b1a6d5df53ca159d53cf2069c8e294cda202db
Ashwin Arora <ashwinarora48@gmail.com>
2023-06-22 16:44:38 +0530

    vault interface updated

--- contracts/interfaces/IVaultManager.sol
+++ contracts/interfaces/IVaultManager.sol
@@ -2,11 +2,7 @@
 pragma solidity ^0.8.0;
 
 interface IVaultManager{
-    function totalVaults() external view returns(uint);
-    function createNewVault(address _tokenAddress) external returns(uint vaultId);
     function depositByToken(address _tokenAddress, address from, uint _amount) external returns (uint vaultId);
     function withdrawByVaultId(uint _vaultId, address to, uint _amount) external;
-    function getVaultBalanceByVaultId(uint _vaultId) external view returns(uint);
-    function getVaultBalanceByToken(address _tokenAddress) external view returns(uint);
-    function vaultIdToVault(uint256 vaultId) external view returns (address);
+    function vaultIdToTokenAddress(uint _vaultId) external view returns (address token);
 }
