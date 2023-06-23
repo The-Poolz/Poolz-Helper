49d85c24a14ca01b1920f53da0c29ac56b86c283
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-06-23 18:09:30 +0300

    added tokenToVaultId to IVaultManager interface

--- contracts/interfaces/IVaultManager.sol
+++ contracts/interfaces/IVaultManager.sol
@@ -5,4 +5,5 @@ interface IVaultManager{
     function depositByToken(address _tokenAddress, address from, uint _amount) external returns (uint vaultId);
     function withdrawByVaultId(uint _vaultId, address to, uint _amount) external;
     function vaultIdToTokenAddress(uint _vaultId) external view returns (address token);
+    function tokenToVaultId(address _tokenAddress) external view returns (uint vaultId);
 }
