93f5294db72e26b4961646f90f13fd2138e7d487
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-08-07 12:05:54 +0300

    added IERC2981 to IVaultManager

--- contracts/interfaces/IVaultManager.sol
+++ contracts/interfaces/IVaultManager.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IVaultManager{
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
+
+interface IVaultManager is IERC2981 {
     function depositByToken(address _tokenAddress, address from, uint _amount) external returns (uint vaultId);
     function withdrawByVaultId(uint _vaultId, address to, uint _amount) external;
     function vaultIdToTokenAddress(uint _vaultId) external view returns (address token);
