5d3020ecef89c40ca27ab8bd60be074f9ec2f0fd
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-10-18 11:56:09 +0300

    update interface

--- contracts/interfaces/IVaultManager.sol
+++ contracts/interfaces/IVaultManager.sol
@@ -4,7 +4,13 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
 interface IVaultManager is IERC2981 {
-    function depositByToken(address _tokenAddress, address from, uint _amount) external returns (uint vaultId);
+    function depositByToken(address _tokenAddress, uint _amount) external returns (uint vaultId);
+    function safeDeposit(
+        address _tokenAddress,
+        uint _amount,
+        address _from,
+        bytes memory _signature
+    ) external returns (uint vaultId);
     function withdrawByVaultId(uint _vaultId, address to, uint _amount) external;
     function vaultIdToTokenAddress(uint _vaultId) external view returns (address token);
     function vaultIdToTradeStartTime(uint256 _vaultId) external view returns (uint256 startTime);
