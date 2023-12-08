45a96ef7e3eab4ffe0b36fb6e8a4524df3eeb6ed
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-12-08 14:32:47 +0200

    try to fix Zip bomb attack

--- scripts/downloadZIP.ts
+++ scripts/downloadZIP.ts
@@ -21,11 +21,25 @@ async function downloadAndExtractZip(url: string, extractPath: string): Promise<
   return new Promise((resolve, reject) => {
     writer.on('finish', () => {
       const zip = new AdmZip(zipPath);
+
+      // Validate the size of the extracted content
+      const entries = zip.getEntries();
+      const totalSize = entries.reduce((size, entry) => size + entry.header.size, 0);
+
+      // Set a threshold for the total size (adjust as needed)
+      const MAX_TOTAL_SIZE = 1000000000; // 1 GB
+
+      if (totalSize > MAX_TOTAL_SIZE) {
+        fs.unlinkSync(zipPath);
+        reject('Exceeded max total size');
+        return;
+      }
+
       // Extract the contents to the specified path
       zip.extractAllTo(extractPath, /*overwrite*/ true);
 
-      // Get the name of the extracted folder (e.g., "LockDealNFT-master")
-      const extractedFolderName = zip.getEntries()[0].entryName.split('/')[0];
+      // Get the name of the extracted folder
+      const extractedFolderName = entries[0].entryName.split('/')[0];
 
       // Remove all words that start with a hyphen from the extracted folder name
       const modifiedFolderName = extractedFolderName.replace(/-[^/]+/g, '');
@@ -35,7 +49,11 @@ async function downloadAndExtractZip(url: string, extractPath: string): Promise<
       fs.unlinkSync(zipPath);
       resolve();
     });
-    writer.on('error', reject);
+
+    writer.on('error', err => {
+      fs.unlinkSync(zipPath);
+      reject(err);
+    });
   });
 }
 
