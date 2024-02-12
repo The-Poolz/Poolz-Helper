ad3da778116f080e3c25383a93189a6667c11288
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2024-02-12 13:44:15 +0200

    add Nameable abstract contract

---
+++ contracts/Nameable.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title Nameable Helper Contract
+ * @dev Abstract contract defines functions to retrieve the name and version of a contract.
+ */
+abstract contract Nameable {
+    /**
+     * @dev Returns the name of the contract.
+     * @return A string representing the name of the contract.
+     */
+    function name() external virtual pure returns(string memory);
+
+    /**
+     * @dev Returns the `Strapi` version of the contract.g
+     * @return A string representin the version of the contract.
+     */
+    function version() external virtual pure returns(string memory);
+}
