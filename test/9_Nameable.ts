1dd681571b3bbc223dff8bf85190409ab6181471
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2024-02-12 14:32:10 +0200

    add tests for Nameable

---
+++ test/9_Nameable.ts
@@ -0,0 +1,27 @@
+import { deployed } from '../scripts/deploy';
+import { Nameable } from '../typechain-types';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+describe('ERC20 Helper tests', function () {
+  let nameable: Nameable;
+  let owner: SignerWithAddress;
+  const name = 'TEST name';
+  const version = '1.0.0';
+
+  before(async () => {
+    [owner] = await ethers.getSigners();
+    nameable = await deployed('Nameable', name, version);
+  });
+
+  it('should return name of contract', async () => {
+    const contractName = await nameable.name();
+    expect(name).to.equal(contractName);
+  });
+
+  it('should return version of contract', async () => {
+    const contractVersion = await nameable.version();
+    expect(version).to.equal(contractVersion);
+  });
+});
